9c5353d4ea8f2f301859de5da97def843ce469e7
ayush <62694274+ayshptk@users.noreply.github.com>
2022-02-11 04:12:57 +0530

    fix example

--- examples/node/nft/mint-nft/index.ts
+++ examples/node/nft/mint-nft/index.ts
@@ -1,27 +1,24 @@
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
 
+
+// instantiate the thirdweb sdk
 const sdk = new ThirdwebSDK(
   new ethers.Wallet(
-    // never share your
+    // NEVER share your private key with anyone else!
     process.env.PKEY as string,
     // we are using rinkeby testnet for the example, you can use anything!
     ethers.getDefaultProvider("rinkeby")
   )
 );
 
-const nftCollection = sdk.getNFTModule("<MODULE_ADDRESS>");
-
-// assign the smart contract address
-const nft_smart_contract_address = "<NFT_CONTRACT_ADDRESS>";
-
-// Instantiate NFT Collection module
-const nft = sdk.getNFTModule(nft_smart_contract_address);
+// instantiate the NFT module (make sure you are on the right chain and have the right contract address)
+const nftCollection = sdk.getNFTModule("<NFT_MODULE_ADDRESS>");
 
 // Minting the NFT asynchronously
 async function mint() {
   console.log(
-    await nft.mint({
+    await nftCollection.mint({
       name: "Hi 👋",
       description: "This is my cool NFT!",
       image: "ipfs://QmbGpe6dJQA9awBbTKEULufp9TTjXw1esVUbZNQrLM57nK",
