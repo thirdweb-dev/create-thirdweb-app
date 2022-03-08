f932ea6310902ae9b7c30908e819efd33b719b40
ayush <62694274+ayshptk@users.noreply.github.com>
2022-03-08 21:21:31 +0530

    nft starter

--- examples/nextjs/react/nft-starter/pages/_app.tsx
+++ examples/nextjs/react/nft-starter/pages/_app.tsx
@@ -1,17 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 
 import { ChakraProvider } from "@chakra-ui/react";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-    <ThirdwebProvider 
-      // grab chain ID at https://chainlist.org
-      desiredChainId={1}>
+      <ThirdwebProvider
+        // grab chain ID at https://chainlist.org
+        desiredChainId={ChainId.Rinkeby}
+      >
         <Component {...pageProps} />
       </ThirdwebProvider>
     </ChakraProvider>
